1dded2dbf51f061a30fac979a56fa018d027ee74
Journel C. Cabrillos <65072908+prothej227@users.noreply.github.com>
2025-05-06 23:54:50 +0800

    Enable CORS for Netlify frontend

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080", 
         "http://127.0.0.1:8080", 
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://mystify-dev.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],   # Allow all methods (POST, GET, etc.)
